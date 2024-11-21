873d31def923fe3e3f4e5bb3f13bedd27f035073
queziards@gmail.com <queziards@gmail.com>
2024-11-21 15:11:42 -0300

    alteracao componentapp - raiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { Router, RouterOutlet} from '@angular/router';
-import { TesteComponent } from './teste/teste.component';
-import { MatButtonModule} from '@angular/material/button';
-import {NgModule} from '@angular/core'
+import { Component,ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdicionarProdutoComponent } from './adicionar-produto/adicionar-produto.component';
+//angular material
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -11,19 +12,32 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {FormsModule} from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
+//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {CommonModule } from '@angular/common';
 import {RouterLink} from '@angular/router';
-import { RouterModule, Routes } from '@angular/router';
-
+import {RouterModule, Routes } from '@angular/router';
+//components 
+import {AdminComponent } from "./admin/admin.component";
+import {HttpClient } from '@angular/common/http';
+import { ListarProdutosComponent } from './listar-produtos/listar-produtos.component';
+import { ItemDialogComponent } from './item-dialog/item-dialog.component';
+import { DetalhesProdutoComponent } from './detalhes-produto/detalhes-produto.component';
+import { InfoProdutosComponent } from './info-produtos/info-produtos.component';
+//import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @Component({
   selector: 'app-root', 
-  standalone: true,
+  standalone: true, 
   imports: [
+   
     RouterOutlet,
-    TesteComponent,
+    HomeComponent,
+    AdicionarProdutoComponent,
     MatButtonModule,
     MatIconModule,
     MatBadgeModule,
@@ -33,28 +47,44 @@ import { RouterModule, Routes } from '@angular/router';
     MatInputModule,
     MatSidenavModule,
     FormsModule,
-    NgbModule,
+    //NgbModule,
     CommonModule,
     RouterLink,
     RouterModule,
+    AdminComponent,
+    ListarProdutosComponent,
+    ItemDialogComponent,
+    MatDialogModule, 
+    MatCardModule,
+    DetalhesProdutoComponent,
+   // BrowserAnimationsModule,
+    CarouselModule,
+    InfoProdutosComponent, 
 ],
+
   templateUrl: './app.component.html',
+  providers: [{ provide: MatDialogRef, useValue: {}}, { provide: MAT_DIALOG_DATA, useValue: {} }, ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'v-ecommerce';
+  constructor(private dialog: MatDialog) {}
+  openItemDialog(): void {
+    const dialogRef = this.dialog.open(ItemDialogComponent, {
+      data: { /* insira os dados aqui, por exemplo, um objeto PeriodicElement */ }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // Aqui você pode lidar com o resultado do diálogo, se necessário
+      console.log('O diálogo foi fechado com resultado: ', result);
+    });
+  }
   opened = false;
   images = [
     'assets/front-3.png',
     'assets/front-2.png'
   ];
 
-  imagesWithCaptions = [
-    { src: 'assets/teste.png', caption: 'Legenda 1' },
-    { src: 'assets/teste.png', caption: 'Legenda 2' },
-    { src: 'assets/teste.png', caption: 'Legenda 3' },
-    { src: 'assets/teste.png', caption: 'Legenda 4' }
-  ];
 
 }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,33 @@
-import { Routes, RouterModule, RouterLink } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginUsuarioComponent } from './login-usuario/login-usuario.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdicionarProdutoComponent } from './adicionar-produto/adicionar-produto.component';
+import { ListarProdutosComponent } from './listar-produtos/listar-produtos.component';
+import { DetalhesProdutoComponent } from './detalhes-produto/detalhes-produto.component';
+import { InfoProdutosComponent } from './info-produtos/info-produtos.component';
+import { ExibicaoHomeComponent } from './exibicao-home/exibicao-home.component';
 
 export const routes: Routes = [
     { path: 'login-usuario', component: LoginUsuarioComponent },
+    { path: '', component: HomeComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'adicionar-produto', component: AdicionarProdutoComponent},
+    { path: 'admin', component: AdminComponent},
+    { path: 'listar-produtos', component: ListarProdutosComponent},
+    { path: 'adicionar-produto/listar-produto', component: ListarProdutosComponent},
+    { path: 'home/:id', component: HomeComponent },
+    { path:  'detalhes-produto/:id', component: DetalhesProdutoComponent},
+    { path: '', component: ExibicaoHomeComponent }, // Rota principal com o carrossel
+    {path:   'info-produtos/:id', component: InfoProdutosComponent},
+    { path: '**', redirectTo: ''}
+     
   ];
+  
 
-  @NgModule({
-    imports: [RouterModule.forRoot(routes), RouterLink],
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
-  export class AppRoutes { }
+export class AppRoutes { }
